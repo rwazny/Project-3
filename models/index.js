4b62c3cdb2f3048156c5ee930ea91419030bf58c
marajita <rajita.maharjan@gmail.com>
2019-03-29 23:02:21 -0400

    added api call and database

---
+++ models/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Meal: require("./meal/meal")
+};
---
+++ routes/api/meal.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const mealController = require("../../controllers/meal/mealController")
+
+
+// Matches with "/api/meals"
+router
+  .route("/")
+  .get(mealController.findAll)
+  .post(mealController.create);
+
+// Matches with "/api/meals/:id"
+router
+  .route("/:id")
+  .get(mealController.findById)
+  .put(mealController.update)
+  .delete(mealController.remove);
+
+module.exports = router;
